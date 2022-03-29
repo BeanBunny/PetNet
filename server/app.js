f2ac8f90c034e50cb86c245b17626b9a1f23831d
Umair <23100053@lums.edu.pk>
2022-03-29 18:27:21 +0500

    MongoDB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,25 @@
 // Import all dependencies & middleware here
 import express from "express";
 import bodyParser from "body-parser";
+import userController from "./controller/user.controller.js";
+import mongodb from "mongodb";
 
-import { userController } from "./user.controller";
+const MongoClient = mongodb.MongoClient;
+const connectionURL = "mongodb://127.0.0.1:27017";
+const dbName = "petnet";
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log(error);
+    }
+
+    console.log("Conneced to MongoDB");
+    const db = client.db(dbName);
+    db.collection("users").insertOne({
+        name: "umair",
+        age: 21,
+    });
+});
 
 // Init an Express App.
 const app = express();
@@ -19,5 +36,5 @@ app.use(bodyParser.json());
 app.use("/", userController);
 // Start Server here
 app.listen(8080, () => {
-    console.log("Server is running on port 8080!");
+    console.log("Server is running on port 8080...");
 });

19ba4ec9aee39a1712264c02ae30051eac9fe48a
Faizan Faisal <faizanf114@gmail.com>
2022-03-31 01:55:01 +0500

    added some validations

--- server/database/schema_test_script.js
+++ server/database/schema_test_script.js
@@ -1,24 +1,48 @@
-const mongoose = require("mongoose");
-const models = require("./schema");
+import mongoose from "mongoose";
+import { models } from "./schema.js";
 const petOwner = models[0];
 const admin = models[1];
 //import more models as schema is made
 
-//On establishing connection, collections from models are automatically added
+//On establishing connection, collections from models are automatically added to db if they dont exist there
 mongoose.connect("mongodb://localhost/petnet", (error, db) => {
   if (error) {
     console.log("DB connection can not be established");
   } else {
-    //script to check admin schema
-    let newAdmin = new admin({
+    //check petowner
+    let testPetOwner = new petOwner({
+      name: "Martin Scorcese",
+      phone: "03009988770", //check regex
+      email: "scorcese@gmail.com",
+      password: "qwertyuiop123$$",
+      pet: {
+        pet_type: "Dog",
+        pet_name: "De Niro",
+      },
+    });
+    testPetOwner
+      .save()
+      .then((res) => {
+        console.log("Pet owner added", res);
+      })
+      .catch((err) => {
+        console.log("Error in adding pet owner", err);
+      });
+
+    //script to check admin
+    let testAdmin = new admin({
       email: "abc@gmail.com",
       password: "qwertyuiop123$$",
     });
-    newAdmin
+    testAdmin
       .save()
-      .then(() => {
-        console.log("New Admin saved");
+      .then((res) => {
+        console.log("New Admin saved:", res);
       })
       .catch((err) => console.log("Err in saving", err));
   }
 });
+
+//Ignore
+//.\mongod.exe --dbpath=C:\Users\HP\mongodb-data
+//C:\Users\HP\mongodb\bin

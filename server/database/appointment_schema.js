f32832048db0fe9d629e334ea4a1433c023dab95
Faizan Faisal <faizanf114@gmail.com>
2022-03-31 16:32:14 +0500

    individual schema files made

---
+++ server/database/appointment_schema.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const appointmentSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  time: {
+    type: Date,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  petowner_id: {
+    type: ObjectId,
+    required: true,
+  },
+  pet_id: {
+    type: ObjectId,
+    required: true,
+  },
+  vet_id: {
+    type: ObjectId,
+    required: true,
+  },
+});
+
+export const appointment_model = mongoose.model(
+  "appointment",
+  appointmentSchema
+);
--- server/database/schema_test_script.js
+++ server/database/schema_test_script.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
-import { models } from "./schema.js";
-const petOwner = models[0];
-const admin = models[1];
+import { petowner_model } from "./petowner_schema.js";
+import { appointment_model } from "./appointment_schema.js";
+import { admin_model } from "./admin_schema.js";
+
+const petOwner = petowner_model;
+const admin = admin_model;
+const appointment = appointment_model;
 //import more models as schema is made
 
 //On establishing connection, collections from models are automatically added to db if they dont exist there

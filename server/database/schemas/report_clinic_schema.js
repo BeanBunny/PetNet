29dca59a6227881563b0421136bf292a661a7a6f
Faizan Faisal <faizanf114@gmail.com>
2022-04-02 20:55:38 +0500

    added schemas for reports

---
+++ server/database/schemas/report_clinic_schema.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const reportClinicSchema = new mongoose.Schema({
+  pet_owner_id: {
+    type: ObjectId,
+    required: true,
+  },
+  report_reason: {
+    type: String,
+    required: true,
+  },
+  vet_id: {
+    type: ObjectId,
+    required: true,
+  },
+});
+
+export const reportClinic_model = mongoose.model(
+  "reported clinic",
+  reportClinicSchema
+);
--- server/database/schemas/schema_test_script.js
+++ server/database/schemas/schema_test_script.js
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 import { petowner_model } from "./petowner_schema.js";
 import { appointment_model } from "./appointment_schema.js";
 import { admin_model } from "./admin_schema.js";
+import { reportPetOwner_model } from "./report_petowner_schema.js";
+import { reportClinic_model } from "./report_clinic_schema.js";
 
 const petOwner = petowner_model;
 const admin = admin_model;
 const appointment = appointment_model;
+const report_petowner = reportPetOwner_model;
+const report_clinic = reportClinic_model;
 //import more models as schema is made
 
 //On establishing connection, collections from models are automatically added to db if they dont exist there
@@ -15,7 +19,7 @@ mongoose.connect("mongodb://localhost/petnet", (error, db) => {
   } else {
     //check petowner
     let testPetOwner = new petOwner({
-      name: "Martin Scorcese",
+      name: "Martin Scorsese",
       phone: "03009988770", //check regex
       email: "scorcese@gmail.com",
       password: "qwertyuiop123$$",
@@ -45,6 +49,19 @@ mongoose.connect("mongodb://localhost/petnet", (error, db) => {
       })
       .catch((err) => console.log("Err in saving", err));
   }
+
+  //script to check report petowner
+  let testReportPetOwner = new report_petowner({
+    pet_owner_id: new mongoose.Types.ObjectId(),
+    report_reason: "Spam appointments",
+    vet_id: new mongoose.Types.ObjectId(),
+  });
+  testReportPetOwner
+    .save()
+    .then((res) => {
+      console.log("New pet owner report saved:", res);
+    })
+    .catch((err) => console.log("Err in saving pet owner report", err));
 });
 
 //Ignore

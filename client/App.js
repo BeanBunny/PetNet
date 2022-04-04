251d4b96c545faf477228809e8cd5618ff23c3c6
QimraHameed <23100277@lums.edu.pk>
2022-04-04 08:01:38 +0500

    text fields to be added

--- client/App.js
+++ client/App.js
@@ -6,19 +6,19 @@ import LoginScreen from "./src/screens/LoginScreen";
 
 // StackNavigator is an object that decides which component we are going to see on the screen
 const navigator = createStackNavigator(
-    {
-        Home: HomeScreen,
-        Login: LoginScreen,
+  {
+    Home: HomeScreen,
+    Login: LoginScreen,
+  },
+  {
+    // initialRouteName is a description or configuration option that tells the navigator what is the first component that
+    // should be displayed when our application first starts up
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "App",
+      cardStyle: { backgroundColor: "#66C4D2" },
     },
-    {
-        // initialRouteName is a description or configuration option that tells the navigator what is the first component that
-        // should be displayed when our application first starts up
-        initialRouteName: "Home",
-        defaultNavigationOptions: {
-            title: "App",
-            cardStyle: { backgroundColor: "#66C4D2" },
-        },
-    }
+  }
 );
 
 export default createAppContainer(navigator);
--- client/src/screens/LoginScreen.js
+++ client/src/screens/LoginScreen.js
@@ -1,29 +1,127 @@
 import React from "react";
 import {
-    Text,
-    StyleSheet,
-    View,
-    Button,
-    TouchableOpacity,
-    Image,
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Image,
+  Pressable,
+  TextInput,
+  ImageBackground,
 } from "react-native";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+import { Button } from "react-native-paper";
 
 const LoginScreen = (props) => {
-    return (
-        <View>
-            <Text style={styles.text}> LOGIN SCREEN </Text>
-        </View>
-    );
+  const [text, onChangeText] = React.useState(null);
+  return (
+    <PaperProvider>
+      <View>
+        {/* <Text style={styles.text}>"Hello"</Text> */}
+        <Image
+          style={styles.image_resize}
+          source={require("../../assets/now.jpg")}
+        />
+        <Text style={styles.text}> PetNet </Text>
+        <ImageBackground
+          resizeMode="contain"
+          style={styles.image_login}
+          source={require("../../assets/login.jpg")}
+        >
+          <Text>Hellp</Text>
+          <TextInput
+            // style={styles.input}
+            onChangeText={onChangeText}
+            value={text}
+            placeholder="Phone Number"
+          />
+          {/* <TextInput
+            style={styles.input}
+            onChangeText={onChangeText}
+            value={text}
+            placeholder="Password"
+          />
+          <Button
+            mode="contained"
+            onPress={() => console.log(value)}
+            style={styles.button}
+          ></Button> */}
+        </ImageBackground>
+      </View>
+    </PaperProvider>
+  );
 };
 
 const styles = StyleSheet.create({
-    text: {
-        fontSize: 70,
-        // marginLeft: 138,
-        // marginTop: 8,
-        color: "white",
-        fontWeight: "bold",
-    },
+  text: {
+    fontSize: 30,
+    marginLeft: 180,
+    marginTop: 8,
+    color: "white",
+    fontWeight: "bold",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    color: "#32627F",
+  },
+  or_text: {
+    fontSize: 30,
+    marginLeft: -10,
+    marginTop: 40,
+    color: "white",
+    fontWeight: "bold",
+  },
+  text_button: {
+    fontSize: 20,
+    marginLeft: 50,
+    marginTop: 7,
+    color: "white",
+    fontWeight: "bold",
+  },
+  image_resize: {
+    // flex: 1,
+    // width: 200,
+    marginTop: 30,
+    height: 200,
+    resizeMode: "contain",
+    paddingLeft: 480,
+    // alignItems: "center",
+    // paddingTop: 200,
+  },
+  image_login: {
+    // flex: 1,
+    // width: 200,
+    marginTop: 50,
+    height: 400,
+    resizeMode: "contain",
+    paddingLeft: 480,
+    // alignItems: "center",
+    // paddingTop: 200,
+  },
+  button: {
+    // the actual contents of the button
+    // alignItems: "center",
+    // justifyContent: "center",
+    // paddingVertical: 25,
+    // paddingHorizontal: 0,
+    // borderRadius: 20,
+    // elevation: 100,
+    marginTop: 50,
+    // marginLeft: 105,
+    // marginRight: 100,
+    backgroundColor: "#32627F",
+    height: "13%",
+
+    width: "70%",
+  },
+  view_button: {
+    alignItems: "center",
+    justifyContent: "center",
+    // color: "black",
+  },
 });
 
 export default LoginScreen;

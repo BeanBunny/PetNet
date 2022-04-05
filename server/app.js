983f1d28dc6bc2b0ac963587746a0c668b192e50
Faizan Faisal <faizanf114@gmail.com>
2022-04-06 02:11:44 +0500

    models set up

--- server/app.js
+++ server/app.js
@@ -2,24 +2,41 @@
 import express from "express";
 import bodyParser from "body-parser";
 import userController from "./controller/user.controller.js";
-import mongodb from "mongodb";
-
-const MongoClient = mongodb.MongoClient;
-const connectionURL = "mongodb://127.0.0.1:27017";
+import mongoose from "mongoose";
+import { models } from "./database/schemas/models.js";
+const connectionURL = "mongodb://127.0.0.1:27017/";
 const dbName = "petnet";
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
-    if (error) {
-        return console.log(error);
-    }
-
-    console.log("Conneced to MongoDB");
-    const db = client.db(dbName);
-    db.collection("users").insertOne({
-        name: "umair",
-        age: 21,
-    });
+//DB connection
+mongoose.connect(connectionURL + dbName);
+const db = mongoose.connection;
+db.on("error", function (err) {
+  console.log("Mongoose default connection error: " + err);
 });
+// connection is disconnected
+db.on("disconnected", function () {
+  console.log("Mongoose default connection disconnected");
+});
+
+// models
+//   .petowner({
+//     name: "Martin Scorsese",
+//     phone: "03009988770", //check regex
+//     email: "scorcese@gmail.com",
+//     password: "qwertyuiop123$$",
+//     pet: {
+//       pet_type: "Dog",
+//       pet_name: "De Niro",
+//     },
+//   })
+//   .save()
+//   .catch((err) => {
+//     console.log("Error in adding pet owner", err);
+//   });
+
+// models.petowner.find({ email: "scorcese@gmail.com" }, (err, res) => {
+//   console.log(res);
+// });
 
 // Init an Express App.
 const app = express();
@@ -36,5 +53,5 @@ app.use(bodyParser.json());
 app.use("/", userController);
 // Start Server here
 app.listen(8080, () => {
-    console.log("Server is running on port 8080...");
+  console.log("Server is running on port 8080...");
 });

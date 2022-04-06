eb7012afdffdca5d5d323d1f6b838dd3c62913f0
BeanBunny <ahmadzubair11111@gmail.com>
2022-04-07 00:29:20 +0500

    Screen

--- client/App.js
+++ client/App.js
@@ -1,21 +1,17 @@
-import "react-native-gesture-handler";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import HomeScreen from "./src/screens/HomeScreen";
+import StartScreen from "./src/screens/StartScreen";
 import LoginScreen from "./src/screens/LoginScreen";
 import SignUpScreen from "./src/screens/SignUpScreen";
 
-// StackNavigator is an object that decides which component we are going to see on the screen
 const navigator = createStackNavigator(
   {
-    Home: HomeScreen,
-    Login: LoginScreen,
-    SignUp: SignUpScreen,
+    StartUp: StartScreen,
+    // Login: LoginScreen,
+    // SignUp: SignUpScreen,
   },
   {
-    // initialRouteName is a description or configuration option that tells the navigator what is the first component that
-    // should be displayed when our application first starts up
-    initialRouteName: "Home",
+    initialRouteName: "StartUp",
     defaultNavigationOptions: {
       title: "App",
       cardStyle: { backgroundColor: "#66C4D2" },

bd48c8442813d4a4f9fdae089393247ffe19c687
QimraHameed <23100277@lums.edu.pk>
2022-04-17 19:18:03 +0500

    add pet create password

--- client/src/screens/petscreens/CreatePassword.js
+++ client/src/screens/petscreens/CreatePassword.js
@@ -2,6 +2,7 @@ import React, { useReducer } from "react";
 import { View, StyleSheet, Image } from "react-native";
 import Input from "../../components/InputBox";
 import AlertButton from "../../components/AlertButton";
+import { LinearGradient } from "expo-linear-gradient";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -17,36 +18,41 @@ const reducer = (state, action) => {
 const CreatePassword = ({ navigation }) => {
   const [state, dispatch] = useReducer(reducer, { New: "", Confirm: "" });
   return (
-    <View>
-      <Image
-        style={styles.img}
-        source={require("../../../assets/Logo1.jpeg")}
-      />
-      <View style={styles.container}>
-        <Input
-          label="New"
-          placeholder="New Password"
-          reducer={[state, dispatch]}
-          style={styles.input}
-          secure={true}
+    <View style={styles.containerbackground}>
+      <LinearGradient
+        colors={["#39B1FB", "#5095FD", "#6877FE", "#8556FE"]}
+        style={{ flex: 1 }}
+      >
+        <Image
+          style={styles.img}
+          source={require("../../../assets/logoNew.png")}
         />
-        <Input
-          label="Confirm"
-          placeholder="Confirm Password"
-          reducer={[state, dispatch]}
-          style={styles.input}
-          secure={true}
+        <View style={styles.container}>
+          <Input
+            label="New"
+            placeholder="New Password"
+            reducer={[state, dispatch]}
+            style={styles.input1}
+            secure={true}
+          />
+          <Input
+            label="Confirm"
+            placeholder="Confirm Password"
+            reducer={[state, dispatch]}
+            style={styles.input}
+            secure={true}
+          />
+        </View>
+        <AlertButton
+          style={styles.button}
+          navigation={navigation}
+          text="Confirm"
+          route="Login"
+          alertText1="Your new password has been created."
+          alertText2="Login with your new password to continue."
+          alertText3="Login!"
         />
-      </View>
-      <AlertButton
-        style={styles.button}
-        navigation={navigation}
-        text="Confirm"
-        route="Login"
-        alert1="Your new password has been created."
-        alert2="Login with your new password to continue."
-        alert3="Login!"
-      />
+      </LinearGradient>
     </View>
   );
 };
@@ -58,12 +64,20 @@ CreatePassword.navigationOptions = () => {
 };
 
 const styles = StyleSheet.create({
+  containerbackground: {
+    flex: 1,
+    backgroundColor: "#66C4D2",
+  },
   img: {
     justifyContent: "center",
     resizeMode: "contain",
     height: "30%",
-    marginHorizontal: "10%",
-    marginTop: "10%",
+    marginLeft: "26%",
+    marginTop: "26%",
+  },
+  input1: {
+    marginHorizontal: "15%",
+    marginTop: "12%",
   },
   input: {
     marginHorizontal: "15%",
@@ -72,13 +86,14 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "white",
     borderRadius: 10,
-    height: "30%",
-    marginTop: "20%",
+    height: "26%",
+    marginTop: "4%",
     marginHorizontal: "10%",
   },
   button: {
     marginTop: "5%",
-    marginHorizontal: "15%",
+    marginLeft: "18%",
+    height: "28%",
   },
 });
 

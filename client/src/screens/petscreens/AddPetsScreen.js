3f3658f15cea3c0c3a0cb4e6738528f298115249
QimraHameed <23100277@lums.edu.pk>
2022-04-17 18:10:34 +0500

    add pet screen updated

--- client/src/screens/petscreens/AddPetsScreen.js
+++ client/src/screens/petscreens/AddPetsScreen.js
@@ -1,9 +1,10 @@
 import React, { useReducer } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Image } from "react-native";
 import TopBar from "../../components/TopBar";
 import CompButton from "../../components/Button";
 import Input from "../../components/InputBox";
 import { Picker } from "@react-native-picker/picker";
+import { LinearGradient } from "expo-linear-gradient";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -19,31 +20,40 @@ const reducer = (state, action) => {
 const AddPetsScreen = () => {
   const [state, dispatch] = useReducer(reducer, { Name: "", Type: "" });
   return (
-    <View>
-      <TopBar text="Add Pet" textStyle={styles.text} style={styles.bar} />
-      <View style={styles.container}>
-        <Input
-          label="Name"
-          placeholder="Pet Name"
-          reducer={[state, dispatch]}
-          style={styles.input}
-          secure={false}
+    <View style={styles.containerbackground}>
+      <LinearGradient
+        colors={["#39B1FB", "#5095FD", "#6877FE", "#8556FE"]}
+        style={{ flex: 1 }}
+      >
+        <TopBar text="Add Pet" textStyle={styles.text} style={styles.bar} />
+        <Image
+          style={styles.img}
+          source={require("../../../assets/logoNew.png")}
         />
-        <Picker
-          selectedValue={state.Type}
-          style={styles.input}
-          onValueChange={(itemValue, itemIndex) =>
-            dispatch({ type: "Type", payload: itemValue })
-          }
-        >
-          <Picker.Item label="--" value="" />
-          <Picker.Item label="Cat" value="Cat" />
-          <Picker.Item label="Dog" value="Dog" />
-          <Picker.Item label="Bird" value="Bird" />
-          <Picker.Item label="Other" value="Other" />
-        </Picker>
-      </View>
-      <CompButton text="Add Pet" style={styles.addbutton} />
+        <View style={styles.container}>
+          <Input
+            label="Name"
+            placeholder="Pet Name"
+            reducer={[state, dispatch]}
+            style={styles.input}
+            secure={false}
+          />
+          <Picker
+            selectedValue={state.Type}
+            style={styles.input}
+            onValueChange={(itemValue, itemIndex) =>
+              dispatch({ type: "Type", payload: itemValue })
+            }
+          >
+            <Picker.Item label="--" value="" />
+            <Picker.Item label="Cat" value="Cat" />
+            <Picker.Item label="Dog" value="Dog" />
+            <Picker.Item label="Bird" value="Bird" />
+            <Picker.Item label="Other" value="Other" />
+          </Picker>
+        </View>
+        <CompButton text="Add Pet" style={styles.addbutton} />
+      </LinearGradient>
     </View>
   );
 };
@@ -55,13 +65,24 @@ AddPetsScreen.navigationOptions = () => {
 };
 
 const styles = StyleSheet.create({
-  text: {
+  containerbackground: {
+    flex: 1,
+    backgroundColor: "#66C4D2",
+  },
+  img: {
+    justifyContent: "center",
+    resizeMode: "contain",
+    height: "65%",
     marginLeft: "25%",
+    marginTop: "-35%",
+  },
+  text: {
+    marginLeft: "31%",
     color: "white",
     fontSize: 30,
   },
   bar: {
-    backgroundColor: "#326273",
+    backgroundColor: "rgba(67, 105, 219, 0.8)",
     marginTop: "5%",
   },
   input: {
@@ -71,16 +92,17 @@ const styles = StyleSheet.create({
   },
 
   container: {
-    backgroundColor: "#326273",
+    backgroundColor: "white",
     marginHorizontal: "5%",
-    marginTop: "10%",
+    marginTop: "-30%",
     padding: "5%",
     borderRadius: 10,
     justifyContent: "center",
   },
   addbutton: {
     marginTop: "5%",
-    marginHorizontal: "30%",
+    marginLeft: "18%",
+    height: "25%",
   },
 });
 

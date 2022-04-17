ed6c4c0741a75c4527b7bc327184db64ff5ce4c4
BeanBunny <ahmadzubair11111@gmail.com>
2022-04-18 01:12:52 +0500

    Fixes

---
+++ client/src/components/Button.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+
+const Button = ({
+  style,
+  text,
+  navigation,
+  routeName,
+  onChange,
+  bool,
+  disabled,
+}) => {
+  return (
+    <View style={style}>
+      <TouchableOpacity
+        onPress={onChange}
+        style={disabled ? styles.disabledContainer : styles.container}
+        disabled={disabled}
+      >
+        <Text style={styles.textStyle}>{text}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#326273",
+    padding: "5%",
+    borderRadius: 20,
+    alignItems: "center",
+  },
+  disabledContainer: {
+    backgroundColor: "#d3d9dc",
+    padding: "5%",
+    borderRadius: 20,
+    alignItems: "center",
+  },
+  textStyle: {
+    fontSize: 15,
+    color: "white",
+  },
+});
+
+export default Button;
---
+++ client/src/components/InputBox.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { TextInput } from "react-native-paper";
+
+const InputBox = ({ label, placeholder, reducer, style, secure }) => {
+  const [state, dispatch] = reducer;
+  return (
+    <View style={style}>
+      <TextInput
+        label={label}
+        value={state[label]}
+        mode="outlined"
+        secureTextEntry={secure}
+        placeholder={placeholder}
+        onChangeText={(val) => dispatch({ type: label, payload: val })}
+      />
+    </View>
+  );
+};
+
+const styles2 = StyleSheet.create({});
+
+export default InputBox;

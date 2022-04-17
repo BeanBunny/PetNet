e532ae6ac2fd72a2540ea96712bdfd690007a9d6
BeanBunny <ahmadzubair11111@gmail.com>
2022-04-18 02:15:30 +0500

    Minor fix

--- client/src/screens/LoginScreen.js
+++ client/src/screens/LoginScreen.js
@@ -32,7 +32,9 @@ const LoginScreen = ({ navigation }) => {
 
   return (
     <View style={{ flex: 1 }}>
-      <View style={{ justifyContent: "center", alignItems: "center" }}>
+      <View
+        style={{ flex: 0.2, justifyContent: "center", alignItems: "center" }}
+      >
         <Image style={styles.img} source={require("../../assets/Logo1.jpeg")} />
       </View>
       <View style={styles.container}>

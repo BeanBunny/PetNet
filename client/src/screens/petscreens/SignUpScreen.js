9052f2a149c348c80a425aa14a1d0b6ea85399ab
QimraHameed <23100277@lums.edu.pk>
2022-04-17 16:53:34 +0500

    sign up screen pet side updated

--- client/src/screens/petscreens/SignUpScreen.js
+++ client/src/screens/petscreens/SignUpScreen.js
@@ -6,269 +6,285 @@ import Input from "../../components/InputBox";
 import ButtonComp from "../../components/Button";
 import ErrorTextComponent from "../../components/ErrorTextComponent";
 import { Context as AuthContext } from "../../context/AuthContext";
+import { LinearGradient } from "expo-linear-gradient";
 import {
-    errorEmail,
-    errorNumber,
-    errorRequired,
-    errorName,
-    errorEqual,
+  errorEmail,
+  errorNumber,
+  errorRequired,
+  errorName,
+  errorEqual,
 } from "../../inputvalidation/validators";
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case "Name":
-            return { ...state, Name: action.payload };
-        case "Email":
-            return { ...state, Email: action.payload };
-        case "Number":
-            return { ...state, Number: action.payload };
-        case "Password":
-            return { ...state, Password: action.payload };
-        case "Confirm":
-            return { ...state, Confirm: action.payload };
-        case "Pet":
-            return { ...state, Pet: action.payload };
-        case "Type":
-            return { ...state, Type: action.payload };
-        case "City":
-            return { ...state, City: action.payload };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "Name":
+      return { ...state, Name: action.payload };
+    case "Email":
+      return { ...state, Email: action.payload };
+    case "Number":
+      return { ...state, Number: action.payload };
+    case "Password":
+      return { ...state, Password: action.payload };
+    case "Confirm":
+      return { ...state, Confirm: action.payload };
+    case "Pet":
+      return { ...state, Pet: action.payload };
+    case "Type":
+      return { ...state, Type: action.payload };
+    case "City":
+      return { ...state, City: action.payload };
+    default:
+      return state;
+  }
 };
 
 // name, phone number, email, password, confirm password
 const SignUpScreen = ({ navigation }) => {
-    const [reducerState, dispatch] = useReducer(reducer, {
-        Name: "",
-        Email: "",
-        Number: "",
-        Password: "",
-        Confirm: "",
-        Pet: "",
-        Type: "",
-        City: "",
-    });
+  const [reducerState, dispatch] = useReducer(reducer, {
+    Name: "",
+    Email: "",
+    Number: "",
+    Password: "",
+    Confirm: "",
+    Pet: "",
+    Type: "",
+    City: "",
+  });
 
-    const errorMsgName = errorName(reducerState.Name);
-    const errorMsgEmail = errorEmail(reducerState.Email);
-    const errorMsgNumber = errorNumber(reducerState.Number);
-    const errorMsgPassword = errorRequired(reducerState.Password, "Password");
-    const errorMsgConfirmPassword = errorRequired(
-        reducerState.Confirm,
-        "Confirm Password"
-    );
-    const errorMsgEqualPassword = errorEqual(
-        reducerState.Password,
-        reducerState.Confirm
-    );
-    const errorMsgPetName = errorRequired(reducerState.Pet, "First Pet's Name");
-    const errorMsgPetType = errorRequired(
-        reducerState.Type,
-        "First Pet's Type"
-    );
-    const errorMsgCity = errorRequired(reducerState.City, "Your City");
+  const errorMsgName = errorName(reducerState.Name);
+  const errorMsgEmail = errorEmail(reducerState.Email);
+  const errorMsgNumber = errorNumber(reducerState.Number);
+  const errorMsgPassword = errorRequired(reducerState.Password, "Password");
+  const errorMsgConfirmPassword = errorRequired(
+    reducerState.Confirm,
+    "Confirm Password"
+  );
+  const errorMsgEqualPassword = errorEqual(
+    reducerState.Password,
+    reducerState.Confirm
+  );
+  const errorMsgPetName = errorRequired(reducerState.Pet, "First Pet's Name");
+  const errorMsgPetType = errorRequired(reducerState.Type, "First Pet's Type");
+  const errorMsgCity = errorRequired(reducerState.City, "Your City");
 
-    const { state, signupPet, clearErrorMessage } = useContext(AuthContext);
+  const { state, signupPet, clearErrorMessage } = useContext(AuthContext);
 
-    navigation.addListener("focus", clearErrorMessage);
-    return (
-        <View style={{ flex: 1 }}>
-            <Headline
-                style={{ marginLeft: "30%", marginTop: "3%", color: "#326273" }}
-            >
-                Pet Owner Sign Up
-            </Headline>
-            {/* <Image
+  navigation.addListener("focus", clearErrorMessage);
+  return (
+    <View style={styles.containerbackground}>
+      <LinearGradient
+        colors={["#39B1FB", "#5095FD", "#6877FE", "#8556FE"]}
+        style={{ flex: 1 }}
+      >
+        <Headline
+          style={{
+            marginLeft: "30%",
+            marginTop: "10%",
+            color: "white",
+            fontWeight: "bold",
+          }}
+        >
+          Pet Owner Sign Up
+        </Headline>
+        {/* <Image
                 style={styles.img}
                 source={require("../../../assets/Logo1.jpeg")}
             /> */}
-            <ScrollView style={styles.container}>
-                <Input
-                    label="Name"
-                    placeholder="Full Name"
-                    reducer={[reducerState, dispatch]}
-                    style={styles.input}
-                    secure={false}
-                />
-                {/* {errorMsgName != null ? (
+        <View style={styles.container}>
+          <Input
+            label="Name"
+            placeholder="Full Name"
+            reducer={[reducerState, dispatch]}
+            style={styles.input1}
+            secure={false}
+          />
+          {/* {errorMsgName != null ? (
                     <ErrorTextComponent error={errorMsgName} />
                 ) : null} */}
-                <Input
-                    label="Number"
-                    placeholder="03XXXXXXXXX"
-                    reducer={[reducerState, dispatch]}
-                    style={styles.input}
-                    secure={false}
-                />
-                {/* {errorMsgNumber != null ? (
+          <Input
+            label="Number"
+            placeholder="03XXXXXXXXX"
+            reducer={[reducerState, dispatch]}
+            style={styles.input}
+            secure={false}
+          />
+          {/* {errorMsgNumber != null ? (
                     <ErrorTextComponent error={errorMsgNumber} />
                 ) : null} */}
-                <Input
-                    label="Email"
-                    placeholder="example@gmail.com"
-                    reducer={[reducerState, dispatch]}
-                    style={styles.input}
-                    secure={false}
-                />
-                {/* {errorMsgEmail != null ? (
+          <Input
+            label="Email"
+            placeholder="example@gmail.com"
+            reducer={[reducerState, dispatch]}
+            style={styles.input}
+            secure={false}
+          />
+          {/* {errorMsgEmail != null ? (
                     <ErrorTextComponent error={errorMsgEmail} />
                 ) : null} */}
 
-                <Input
-                    label="Password"
-                    placeholder="Password"
-                    reducer={[reducerState, dispatch]}
-                    style={styles.input}
-                    secure={true}
-                />
-                {/* {errorMsgPassword != null ? (
+          <Input
+            label="Password"
+            placeholder="Password"
+            reducer={[reducerState, dispatch]}
+            style={styles.input}
+            secure={true}
+          />
+          {/* {errorMsgPassword != null ? (
                     <ErrorTextComponent error={errorMsgPassword} />
                 ) : null} */}
-                <Input
-                    label="Confirm"
-                    placeholder="Confirm Password"
-                    reducer={[reducerState, dispatch]}
-                    style={styles.input}
-                    secure={true}
-                />
-                {/* {errorMsgConfirmPassword != null ? (
+          <Input
+            label="Confirm"
+            placeholder="Confirm Password"
+            reducer={[reducerState, dispatch]}
+            style={styles.input}
+            secure={true}
+          />
+          {/* {errorMsgConfirmPassword != null ? (
                     <ErrorTextComponent error={errorMsgConfirmPassword} />
                 ) : null}
                 {errorMsgEqualPassword != null ? (
                     <ErrorTextComponent error={errorMsgEqualPassword} />
                 ) : null} */}
-                <Picker
-                    selectedValue={reducerState.City}
-                    style={styles.input}
-                    onValueChange={(itemValue, itemIndex) =>
-                        dispatch({ type: "City", payload: itemValue })
-                    }
-                >
-                    <Picker.Item label="City" value="" />
-                    <Picker.Item label="Lahore" value="Lahore" />
-                    <Picker.Item label="Karachi" value="Karachi" />
-                    <Picker.Item label="Islamabad" value="Islamabad" />
-                </Picker>
-                {/* {errorMsgCity != null ? (
+          <Picker
+            selectedValue={reducerState.City}
+            style={styles.input}
+            onValueChange={(itemValue, itemIndex) =>
+              dispatch({ type: "City", payload: itemValue })
+            }
+          >
+            <Picker.Item label="City" value="" />
+            <Picker.Item label="Lahore" value="Lahore" />
+            <Picker.Item label="Karachi" value="Karachi" />
+            <Picker.Item label="Islamabad" value="Islamabad" />
+          </Picker>
+          {/* {errorMsgCity != null ? (
                     <ErrorTextComponent error={errorMsgCity} />
                 ) : null} */}
-                <Input
-                    label="Pet"
-                    placeholder="Pet Name"
-                    reducer={[reducerState, dispatch]}
-                    style={styles.input}
-                    secure={false}
-                />
-                {/* {errorMsgPetName != null ? (
+          <Input
+            label="Pet"
+            placeholder="Pet Name"
+            reducer={[reducerState, dispatch]}
+            style={styles.input}
+            secure={false}
+          />
+          {/* {errorMsgPetName != null ? (
                     <ErrorTextComponent error={errorMsgPetName} />
                 ) : null} */}
-                <Picker
-                    selectedValue={reducerState.Type}
-                    style={styles.input}
-                    onValueChange={(itemValue, itemIndex) =>
-                        dispatch({ type: "Type", payload: itemValue })
-                    }
-                >
-                    <Picker.Item label="Pet type" value="" />
-                    <Picker.Item label="Cat" value="Cat" />
-                    <Picker.Item label="Dog" value="Dog" />
-                    <Picker.Item label="Bird" value="Bird" />
-                    <Picker.Item label="Other" value="Other" />
-                </Picker>
-                {/* {errorMsgPetType != null ? (
+          <Picker
+            selectedValue={reducerState.Type}
+            style={styles.input}
+            onValueChange={(itemValue, itemIndex) =>
+              dispatch({ type: "Type", payload: itemValue })
+            }
+          >
+            <Picker.Item label="Pet type" value="" />
+            <Picker.Item label="Cat" value="Cat" />
+            <Picker.Item label="Dog" value="Dog" />
+            <Picker.Item label="Bird" value="Bird" />
+            <Picker.Item label="Other" value="Other" />
+          </Picker>
+          {/* {errorMsgPetType != null ? (
                     <ErrorTextComponent error={errorMsgPetType} />
                 ) : null} */}
-            </ScrollView>
-            {state.errorMessage ? (
-                <ErrorTextComponent error={state.errorMessage} />
-            ) : null}
+          {state.errorMessage ? (
+            <ErrorTextComponent error={state.errorMessage} />
+          ) : null}
 
-            {errorMsgName ? (
-                <ErrorTextComponent error={errorMsgName} />
-            ) : errorMsgNumber ? (
-                <ErrorTextComponent error={errorMsgNumber} />
-            ) : errorMsgEmail ? (
-                <ErrorTextComponent error={errorMsgEmail} />
-            ) : errorMsgPassword ? (
-                <ErrorTextComponent error={errorMsgPassword} />
-            ) : errorMsgConfirmPassword ? (
-                <ErrorTextComponent error={errorMsgConfirmPassword} />
-            ) : errorMsgEqualPassword ? (
-                <ErrorTextComponent error={errorMsgEqualPassword} />
-            ) : errorMsgCity ? (
-                <ErrorTextComponent error={errorMsgCity} />
-            ) : errorMsgPetName ? (
-                <ErrorTextComponent error={errorMsgPetName} />
-            ) : errorMsgPetType ? (
-                <ErrorTextComponent error={errorMsgPetType} />
-            ) : null}
-
-            {errorMsgEmail ||
-            errorMsgPassword ||
-            errorMsgCity ||
-            errorMsgConfirmPassword ||
-            errorMsgEqualPassword ||
-            errorMsgName ||
-            errorMsgNumber ||
-            errorMsgPassword ||
-            errorMsgPetName ||
-            errorMsgPetType ? (
-                <ButtonComp text="Login" style={styles.button} disabled />
-            ) : (
-                <ButtonComp
-                    text="Login"
-                    style={styles.button}
-                    disabled={false}
-                    onChange={() =>
-                        signupPet({
-                            name: reducerState.Name,
-                            email: reducerState.Email,
-                            phone: reducerState.Number,
-                            password: reducerState.Password,
-                            pet: {
-                                petName: reducerState.Pet,
-                                petType: reducerState.Type,
-                            },
-                            location: reducerState.City,
-                            isVet: state.isVet,
-                        })
-                    }
-                />
-            )}
+          {errorMsgName ? (
+            <ErrorTextComponent error={errorMsgName} />
+          ) : errorMsgNumber ? (
+            <ErrorTextComponent error={errorMsgNumber} />
+          ) : errorMsgEmail ? (
+            <ErrorTextComponent error={errorMsgEmail} />
+          ) : errorMsgPassword ? (
+            <ErrorTextComponent error={errorMsgPassword} />
+          ) : errorMsgConfirmPassword ? (
+            <ErrorTextComponent error={errorMsgConfirmPassword} />
+          ) : errorMsgEqualPassword ? (
+            <ErrorTextComponent error={errorMsgEqualPassword} />
+          ) : errorMsgCity ? (
+            <ErrorTextComponent error={errorMsgCity} />
+          ) : errorMsgPetName ? (
+            <ErrorTextComponent error={errorMsgPetName} />
+          ) : errorMsgPetType ? (
+            <ErrorTextComponent error={errorMsgPetType} />
+          ) : null}
         </View>
-    );
+        {errorMsgEmail ||
+        errorMsgPassword ||
+        errorMsgCity ||
+        errorMsgConfirmPassword ||
+        errorMsgEqualPassword ||
+        errorMsgName ||
+        errorMsgNumber ||
+        errorMsgPassword ||
+        errorMsgPetName ||
+        errorMsgPetType ? (
+          <ButtonComp text="Login" style={styles.button} disabled />
+        ) : (
+          <ButtonComp
+            text="Login"
+            style={styles.button}
+            disabled={false}
+            onChange={() =>
+              signupPet({
+                name: reducerState.Name,
+                email: reducerState.Email,
+                phone: reducerState.Number,
+                password: reducerState.Password,
+                pet: {
+                  petName: reducerState.Pet,
+                  petType: reducerState.Type,
+                },
+                location: reducerState.City,
+                isVet: state.isVet,
+              })
+            }
+          />
+        )}
+      </LinearGradient>
+    </View>
+  );
 };
 
 SignUpScreen.navigationOptions = () => {
-    return {
-        headerShown: false,
-    };
+  return {
+    headerShown: false,
+  };
 };
 
 const styles = StyleSheet.create({
-    img: {
-        justifyContent: "center",
-        resizeMode: "contain",
-        height: "15%",
-        marginHorizontal: "10%",
-        marginTop: "10%",
-    },
-    input: {
-        marginHorizontal: "15%",
-        marginVertical: "5%",
-    },
-    container: {
-        backgroundColor: "white",
-        borderRadius: 10,
-        height: "63%",
-        marginTop: "5%",
-        marginHorizontal: "10%",
-    },
-    button: {
-        marginTop: "5%",
-        marginHorizontal: "15%",
-    },
+  containerbackground: {
+    flex: 1,
+    backgroundColor: "#66C4D2",
+  },
+  img: {
+    justifyContent: "center",
+    resizeMode: "contain",
+    height: "15%",
+    marginHorizontal: "10%",
+    marginTop: "10%",
+  },
+  input: {
+    marginHorizontal: "15%",
+    marginTop: "5%",
+  },
+  input1: {
+    marginHorizontal: "15%",
+    marginTop: "8%",
+  },
+  container: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    height: "75%",
+    marginTop: "5%",
+    marginHorizontal: "10%",
+  },
+  button: {
+    marginTop: "5%",
+    marginLeft: "18%",
+    height: "25%",
+  },
 });
 
 export default SignUpScreen;
--- client/src/screens/vetscreens/VetSignUpScreen.js
+++ client/src/screens/vetscreens/VetSignUpScreen.js
@@ -6,149 +6,146 @@ import ButtonComp from "../../components/Button";
 import { Context as AuthContext } from "../../context/AuthContext";
 import ErrorTextComponent from "../../components/ErrorTextComponent";
 import {
-    errorEmail,
-    errorNumber,
-    errorRequired,
-    errorName,
-    errorEqual,
-    errorPVMC,
+  errorEmail,
+  errorNumber,
+  errorRequired,
+  errorName,
+  errorEqual,
+  errorPVMC,
 } from "../../inputvalidation/validators";
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case "Name":
-            return { ...state, Name: action.payload };
-        case "PVMC":
-            return { ...state, PVMC: action.payload };
-        case "Gender":
-            return { ...state, Gender: action.payload };
-        case "Father":
-            return { ...state, Father: action.payload };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "Name":
+      return { ...state, Name: action.payload };
+    case "PVMC":
+      return { ...state, PVMC: action.payload };
+    case "Gender":
+      return { ...state, Gender: action.payload };
+    case "Father":
+      return { ...state, Father: action.payload };
+    default:
+      return state;
+  }
 };
 
 const VetSignUpScreen = ({ navigation }) => {
-    const [reducerState, dispatch] = useReducer(reducer, {
-        Name: "",
-        PVMC: "",
-        Gender: "",
-        Father: "",
-    });
-    const errorMsgPVMC = errorPVMC(reducerState.PVMC);
-    const errorMsgName = errorName(reducerState.Name);
-    const errorMsgGender = errorRequired(reducerState.Gender, "Gender");
-    const errorMsgFather = errorRequired(reducerState.Father, "Father");
-    const { state, signupVet } = useContext(AuthContext);
-    return (
-        <View style={{ flex: 1 }}>
-            <Image
-                style={styles.img}
-                source={require("../../../assets/Logo1.jpeg")}
-            />
-            <ScrollView style={styles.container}>
-                <Input
-                    label="Name"
-                    placeholder="Full Name"
-                    reducer={[reducerState, dispatch]}
-                    style={styles.input}
-                    secure={false}
-                />
-                {errorMsgName != null ? (
-                    <ErrorTextComponent error={errorMsgName} />
-                ) : null}
-                <Input
-                    label="PVMC"
-                    placeholder="PVMC Registration Number"
-                    reducer={[reducerState, dispatch]}
-                    style={styles.input}
-                    secure={false}
-                />
-                {errorMsgPVMC != null ? (
-                    <ErrorTextComponent error={errorMsgPVMC} />
-                ) : null}
-                <Picker
-                    selectedValue={reducerState.Gender}
-                    style={styles.input}
-                    onValueChange={(itemValue, itemIndex) =>
-                        dispatch({ type: "Gender", payload: itemValue })
-                    }
-                >
-                    <Picker.Item label="Gender" value="" />
-                    <Picker.Item label="Male" value="M" />
-                    <Picker.Item label="Female" value="F" />
-                </Picker>
-                {errorMsgGender != null ? (
-                    <ErrorTextComponent error={errorMsgGender} />
-                ) : null}
-                <Input
-                    label="Father"
-                    placeholder="Father's Name"
-                    reducer={[reducerState, dispatch]}
-                    style={styles.input}
-                    secure={false}
-                />
-                {errorMsgFather != null ? (
-                    <ErrorTextComponent error={errorMsgFather} />
-                ) : null}
-            </ScrollView>
-            {errorMsgFather ||
-            errorMsgName ||
-            errorMsgGender ||
-            errorMsgPVMC ? (
-                <ButtonComp text="Continue" style={styles.button} disabled />
-            ) : (
-                <ButtonComp
-                    text="Continue"
-                    style={styles.button}
-                    disabled={false}
-                    onChange={() =>
-                        navigation.navigate("clinicSignUp", {
-                            pvmc_reg: {
-                                name: reducerState.Name,
-                                gender: reducerState.Gender,
-                                reg_num: reducerState.PVMC,
-                                father_name: reducerState.Father,
-                            },
-                            isVet: state.isVet,
-                        })
-                    }
-                />
-            )}
-        </View>
-    );
+  const [reducerState, dispatch] = useReducer(reducer, {
+    Name: "",
+    PVMC: "",
+    Gender: "",
+    Father: "",
+  });
+  const errorMsgPVMC = errorPVMC(reducerState.PVMC);
+  const errorMsgName = errorName(reducerState.Name);
+  const errorMsgGender = errorRequired(reducerState.Gender, "Gender");
+  const errorMsgFather = errorRequired(reducerState.Father, "Father");
+  const { state, signupVet } = useContext(AuthContext);
+  return (
+    <View style={{ flex: 1 }}>
+      <Image
+        style={styles.img}
+        source={require("../../../assets/Logo1.jpeg")}
+      />
+      <View style={styles.container}>
+        <Input
+          label="Name"
+          placeholder="Full Name"
+          reducer={[reducerState, dispatch]}
+          style={styles.input}
+          secure={false}
+        />
+        {errorMsgName != null ? (
+          <ErrorTextComponent error={errorMsgName} />
+        ) : null}
+        <Input
+          label="PVMC"
+          placeholder="PVMC Registration Number"
+          reducer={[reducerState, dispatch]}
+          style={styles.input}
+          secure={false}
+        />
+        {errorMsgPVMC != null ? (
+          <ErrorTextComponent error={errorMsgPVMC} />
+        ) : null}
+        <Picker
+          selectedValue={reducerState.Gender}
+          style={styles.input}
+          onValueChange={(itemValue, itemIndex) =>
+            dispatch({ type: "Gender", payload: itemValue })
+          }
+        >
+          <Picker.Item label="Gender" value="" />
+          <Picker.Item label="Male" value="M" />
+          <Picker.Item label="Female" value="F" />
+        </Picker>
+        {errorMsgGender != null ? (
+          <ErrorTextComponent error={errorMsgGender} />
+        ) : null}
+        <Input
+          label="Father"
+          placeholder="Father's Name"
+          reducer={[reducerState, dispatch]}
+          style={styles.input}
+          secure={false}
+        />
+        {errorMsgFather != null ? (
+          <ErrorTextComponent error={errorMsgFather} />
+        ) : null}
+      </View>
+      {errorMsgFather || errorMsgName || errorMsgGender || errorMsgPVMC ? (
+        <ButtonComp text="Continue" style={styles.button} disabled />
+      ) : (
+        <ButtonComp
+          text="Continue"
+          style={styles.button}
+          disabled={false}
+          onChange={() =>
+            navigation.navigate("clinicSignUp", {
+              pvmc_reg: {
+                name: reducerState.Name,
+                gender: reducerState.Gender,
+                reg_num: reducerState.PVMC,
+                father_name: reducerState.Father,
+              },
+              isVet: state.isVet,
+            })
+          }
+        />
+      )}
+    </View>
+  );
 };
 
 VetSignUpScreen.navigationOptions = () => {
-    return {
-        headerShown: false,
-    };
+  return {
+    headerShown: false,
+  };
 };
 
 const styles = StyleSheet.create({
-    img: {
-        justifyContent: "center",
-        resizeMode: "contain",
-        height: "15%",
-        marginHorizontal: "10%",
-        marginTop: "10%",
-    },
-    input: {
-        marginHorizontal: "15%",
-        marginVertical: "5%",
-    },
-    container: {
-        backgroundColor: "white",
-        borderRadius: 10,
-        height: "63%",
-        marginTop: "5%",
-        marginHorizontal: "10%",
-    },
-    button: {
-        marginTop: "5%",
-        marginHorizontal: "15%",
-    },
+  img: {
+    justifyContent: "center",
+    resizeMode: "contain",
+    height: "15%",
+    marginHorizontal: "10%",
+    marginTop: "10%",
+  },
+  input: {
+    marginHorizontal: "15%",
+    marginVertical: "5%",
+  },
+  container: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    height: "63%",
+    marginTop: "5%",
+    marginHorizontal: "10%",
+  },
+  button: {
+    marginTop: "5%",
+    marginHorizontal: "15%",
+  },
 });
 
 export default VetSignUpScreen;
